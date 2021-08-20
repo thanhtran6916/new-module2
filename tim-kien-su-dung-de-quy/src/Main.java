7098acc3d348ace680688abf96f7b6ea822208b5
ThanhTran <tranvanthanhtran.tt@gmail.com>
2021-08-20 16:05:25 +0700

    first commit

---
+++ tim-kien-su-dung-de-quy/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arrNumber = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int left = 0;
+        int right = arrNumber.length;
+        System.out.println(BinarySearch(arrNumber, 8, left, right));;
+
+    }
+
+    public static int BinarySearch(int[] arrNumber, int value, int left, int right) {
+        int middle = (left + right) / 2;
+        if (arrNumber[middle] == value) {
+            return middle;
+        }
+        if (value < arrNumber[middle]) {
+            right = middle - 1;
+        }
+        if (value > arrNumber[middle]) {
+            left = middle + 1;
+        }
+        return BinarySearch(arrNumber, value, left, right);
+    }
+
+
+}
